6be9029562e96042da3409d9853d2c29d42105be
gulini32 <gulini32@gmail.com>
2024-02-15 10:44:39 +0200

    part3

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,10 +1,13 @@
 import React, {useEffect} from 'react';
 import {useForm} from "react-hook-form";
 import {carService} from "../services/carService";
+import {joiResolver} from "@hookform/resolvers/joi";
+import {carValidator} from "../validators/carValidator";
 
 const CarForm = ({setTrigger, carForUpdate, setCarForUpdate}) => {
     const {register, reset, handleSubmit,formState:{isValid, errors}, setValue} = useForm({
-       mode:'all'
+       mode:'all',
+       resolver:joiResolver(carValidator)
     });
 
     useEffect(() => {
@@ -30,27 +33,36 @@ const CarForm = ({setTrigger, carForUpdate, setCarForUpdate}) => {
 
     return (
         <form onSubmit={handleSubmit(carForUpdate ? update : save)}>
-            <input type={"text"} placeholder={'brand'} {...register('brand', {
-                pattern: {
-                    value:/^[a-zA-Zа-яА-яёЁіІїЇєЄҐґ]{1,20}$/,
-                    message:'min 1 max 12 character'
-                }
-            })}/>
-            <input type={"text"} placeholder={'price'} {...register('price', {
-                valueAsNumber: true,
-                min:{value:0, message:'min o'},
-                max: {value:1000000, message: 'max 1000000'}
-            })}/>
-            <input type={"text"} placeholder={'year'} {...register('year', {
-                valueAsNumber: true,
-                min:{value:1990, message: 'min 1990'},
-                max:{value:new Date().getFullYear(), message:'max current year'}
-            })}/>
+            <input type={"text"} placeholder={'brand'} {...register('brand', {})}/>
+            <input type={"text"} placeholder={'price'} {...register('price', {valueAsNumber:true})}/>
+            <input type={"text"} placeholder={'year'} {...register('year', {valueAsNumber:true})}/>
             <button disabled={!isValid}>{carForUpdate ? 'update' : 'save'}</button>
             {errors.brand&&<div>{errors.brand.message}</div>}
             {errors.price&&<div>{errors.price.message}</div>}
             {errors.year&&<div>{errors.year.message}</div>}
         </form>
+        // <form onSubmit={handleSubmit(carForUpdate ? update : save)}>
+        //     <input type={"text"} placeholder={'brand'} {...register('brand', {
+        //         pattern: {
+        //             value:/^[a-zA-Zа-яА-яёЁіІїЇєЄҐґ]{1,20}$/,
+        //             message:'min 1 max 12 character'
+        //         }
+        //     })}/>
+        //     <input type={"text"} placeholder={'price'} {...register('price', {
+        //         valueAsNumber: true,
+        //         min:{value:0, message:'min o'},
+        //         max: {value:1000000, message: 'max 1000000'}
+        //     })}/>
+        //     <input type={"text"} placeholder={'year'} {...register('year', {
+        //         valueAsNumber: true,
+        //         min:{value:1990, message: 'min 1990'},
+        //         max:{value:new Date().getFullYear(), message:'max current year'}
+        //     })}/>
+        //     <button disabled={!isValid}>{carForUpdate ? 'update' : 'save'}</button>
+        //     {errors.brand&&<div>{errors.brand.message}</div>}
+        //     {errors.price&&<div>{errors.price.message}</div>}
+        //     {errors.year&&<div>{errors.year.message}</div>}
+        // </form>
     );
 };
 
