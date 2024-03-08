ce5a6f5b2e89a1cb79b822edd9627f11db3eac8e
gulini32 <gulini32@gmail.com>
2024-03-08 12:13:27 +0200

    lesson6.part1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {ComponentOne} from "./components/ComponentOne";
+import {ComponentTwo} from "./components/ComponentTwo";
+import {useSelector} from "react-redux";
 
 const App = () => {
+    const {count} = useSelector(state => state.counter1);
+    const {count:count2} = useSelector(state => state.counter2);
+
     return (
         <div>
-            App
+            <div>
+                count: {count}
+            </div>
+            count2: {count2}
+
+
+            <ComponentOne/>
+            <ComponentTwo/>
         </div>
     );
 };
---
+++ src/components/ComponentOne.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {useDispatch} from "react-redux";
+import {counterActions} from "../store/slices/counterSlice";
+
+const ComponentOne = () => {
+    const dispatch = useDispatch();
+    return (
+        <div>
+            <button onClick={()=>dispatch(counterActions.inc())}>inc</button>
+            <button onClick={()=>dispatch(counterActions.dec())}>dec</button>
+            <button onClick={()=>dispatch(counterActions.reset())}>reset</button>
+        </div>
+    );
+};
+
+export {
+    ComponentOne
+};
